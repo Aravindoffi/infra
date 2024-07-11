3ef7cdf7610521ba322c44eb40ff9e1a68ba105e
Aravindoffi <108566027+Aravindoffi@users.noreply.github.com>
2024-07-11 11:07:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -86,5 +86,5 @@ app.put('/update/:id', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Server running at 3000/`);
 });
